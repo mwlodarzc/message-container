83be9c06f7ae7f888abd382a01a6333fd1e5d1a3
mwlodarzc <mwlodarzc@gmail.com>
2022-04-12 23:57:13 +0200

    Comments

--- inc/Comparator.hh
+++ inc/Comparator.hh
@@ -2,7 +2,9 @@
 #define COMPARATOR_HH
 #include "Position.hh"
 #include "IndexedLetter.hh"
-
+/*!
+ * @brief Comparator specified for exercise purpose. It takes Position object reference of IndexedLetter class as paremters and compares their value.
+ */
 class IdLetComparator
 {
 public:
